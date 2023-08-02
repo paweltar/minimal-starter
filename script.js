01f75c9e5dd7502b37407a6d9f859dd181f8d46d
Paweł Targoński <paweltar@gmail.com>
2023-08-02 10:35:53 +0000

    Add OrbitControls.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 // Scene
 const scene = new THREE.Scene();
@@ -17,15 +18,24 @@ document.body.appendChild(renderer.domElement);
 
 // Basic Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 'green' });
+const material = new THREE.MeshBasicMaterial({
+  color: "green",
+  wireframe: true,
+});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+// Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
 function animate() {
   requestAnimationFrame(animate);
 
   cube.rotation.x += 0.005;
   cube.rotation.y += 0.005;
+
+  controls.update();
 
   renderer.render(scene, camera);
 }
